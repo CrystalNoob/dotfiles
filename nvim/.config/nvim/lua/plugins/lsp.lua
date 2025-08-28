2e782818cc89f7f792e23ac6007544e9fa3cacfa
CrystalNoob <69108822+CrystalNoob@users.noreply.github.com>
2025-08-28 10:48:20 +0700

    feat: update nvim config

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,109 +1,111 @@
 local function setup()
-    require("lazydev").setup({
-        library = {
-            "lazy.nvim",
-            { path = "${3rd}/luv/library", words = { "vim%.uv" }, },
-        },
-        integrations = {
-            lspconfig = true,
-            cmp = true,
-            coq = false,
-        },
+	require("lazydev").setup({
+		library = {
+			"lazy.nvim",
+			{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+		},
+		integrations = {
+			lspconfig = true,
+			cmp = true,
+			coq = false,
+		},
 
-        enabled = function(root_dir)
-            return (not vim.uv.fs_stat(root_dir .. "/.luarc.json")) and
-                (vim.g.lazydev_enabled == nil and true or vim.g.lazydev_enabled)
-        end,
-    })
+		enabled = function(root_dir)
+			return (not vim.uv.fs_stat(root_dir .. "/.luarc.json"))
+				and (vim.g.lazydev_enabled == nil and true or vim.g.lazydev_enabled)
+		end,
+	})
 
-    local lspconfig = require("lspconfig")
-    --Enable (broadcasting) snippet capability for completion
-    local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
-    capabilities.textDocument.completion.completionItem.snippetSupport = true
+	local lspconfig = require("lspconfig")
+	--Enable (broadcasting) snippet capability for completion
+	local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
+	capabilities.textDocument.completion.completionItem.snippetSupport = true
 
-    lspconfig.tailwindcss.setup({
-        ft = { "typescriptreact", "javascriptreact", "vue", "svelte", "html" }
-    })
+	lspconfig.bashls.setup({})
 
-    lspconfig.lua_ls.setup({
-        settings = {
-            Lua = {}
-        }
-    })
+	lspconfig.tailwindcss.setup({
+		ft = { "typescriptreact", "javascriptreact", "vue", "svelte", "html" },
+	})
 
-    lspconfig.clangd.setup({
-        capabilities = capabilities
-    })
+	lspconfig.lua_ls.setup({
+		settings = {
+			Lua = {},
+		},
+	})
 
-    lspconfig.gopls.setup({
-        settings = {
-            gopls = {
-                staticcheck = true,
-            }
-        }
-    })
+	lspconfig.clangd.setup({
+		capabilities = capabilities,
+	})
 
-    lspconfig.basedpyright.setup({})
+	lspconfig.gopls.setup({
+		settings = {
+			gopls = {
+				staticcheck = true,
+			},
+		},
+	})
 
-    lspconfig.jdtls.setup({
-        root_dir = function()
-            local dir = vim.fs.dirname(vim.fs.find({ ".git" }, { upward = true })[1])
-            if dir == nil then
-                dir = vim.fn.getcwd()
-            end
-            return dir
-        end
-    })
+	lspconfig.basedpyright.setup({})
 
-    lspconfig.cssls.setup({
-        capabilities = capabilities,
-    })
+	lspconfig.jdtls.setup({
+		root_dir = function()
+			local dir = vim.fs.dirname(vim.fs.find({ ".git" }, { upward = true })[1])
+			if dir == nil then
+				dir = vim.fn.getcwd()
+			end
+			return dir
+		end,
+	})
 
-    lspconfig.ts_ls.setup({})
+	lspconfig.cssls.setup({
+		capabilities = capabilities,
+	})
 
-    lspconfig.yamlls.setup({
-        telemetry = false
-    })
+	lspconfig.ts_ls.setup({})
 
-    lspconfig.html.setup({
-        capabilities = capabilities,
-    })
+	lspconfig.yamlls.setup({
+		telemetry = false,
+	})
 
-    lspconfig.jsonls.setup({
-        settings = {
-            json = {
-                schemas = require("schemastore").json.schemas(),
-                validate = {
-                    enable = true
-                }
-            },
-        },
-        capabilities = capabilities,
-    })
+	lspconfig.html.setup({
+		capabilities = capabilities,
+	})
 
-    lspconfig.docker_compose_language_service.setup({})
+	lspconfig.jsonls.setup({
+		settings = {
+			json = {
+				schemas = require("schemastore").json.schemas(),
+				validate = {
+					enable = true,
+				},
+			},
+		},
+		capabilities = capabilities,
+	})
 
-    lspconfig.dockerls.setup({
-        settings = {
-            docker = {
-                languageserver = {
-                    formatter = {
-                        ignoreMultilineInstructions = true,
-                    },
-                },
-            }
-        }
-    })
+	lspconfig.docker_compose_language_service.setup({})
+
+	lspconfig.dockerls.setup({
+		settings = {
+			docker = {
+				languageserver = {
+					formatter = {
+						ignoreMultilineInstructions = true,
+					},
+				},
+			},
+		},
+	})
 end
 
 return {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-        "williamboman/mason.nvim",
-        "williamboman/mason-lspconfig.nvim",
-        "hrsh7th/cmp-nvim-lsp",
-        "folke/lazydev.nvim",
-        "b0o/schemastore.nvim",
-    },
-    config = setup
+	"neovim/nvim-lspconfig",
+	dependencies = {
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
+		"hrsh7th/cmp-nvim-lsp",
+		"folke/lazydev.nvim",
+		"b0o/schemastore.nvim",
+	},
+	config = setup,
 }
