6802a90a5f228212b22254fbc206daf5b24e1ac4
CrystalNoob <69108822+CrystalNoob@users.noreply.github.com>
2026-04-06 15:23:13 +0700

    fix(nvim): update nvim-treesitter config

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,22 +1,5 @@
 local function setup()
-    require("nvim-treesitter").setup({
-        ensure_installed = { "c", "css", "cpp", "html", "lua", "markdown", "markdown_inline", "query", "vim", "vimdoc" },
-        sync_install = false,
-        auto_install = true,
-        ignore_install = {},
-        highlight = {
-            enable = true,
-            disable = function(_, buf)
-                local max_filesize = 1024 * 1024 -- 1 MB
-                local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(buf))
-                if ok and stats and stats.size > max_filesize then
-                    return true
-                end
-            end,
-            additional_vim_regex_highlighting = false,
-        },
-        modules = {},
-    })
+    require("nvim-treesitter").install { "c", "cpp", "gitconfig", "go", "lua", "markdown", "markdown_inline", "zsh" }
 
     require("nvim-autopairs").setup({
         disable_filetype = {
